4385d3acc934532293cc1479d7dd44ab17899c24
Valerio Persiani <persianivalerio0@gmail.com>
2024-12-17 14:11:29 +0100

    Initial Spring Boot project

---
+++ src/main/java/com/pizzeria/pizzeria_menu/PizzeriaMenuApplication.java
@@ -0,0 +1,13 @@
+package com.pizzeria.pizzeria_menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaMenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pizzeria/pizzeria_menu/PizzeriaMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzeria.pizzeria_menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
